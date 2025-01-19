223ce36c3f9df8e1c660e66ccec7d54e6e2a72e2
kabosu-s <sa_eki12@hotmail.com>
2025-01-19 23:56:16 +0900

    SSGでビルドするように設定

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  output: 'export',
+  // SSGではnext/imageが利用できないためunoptimizedで割愛
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
